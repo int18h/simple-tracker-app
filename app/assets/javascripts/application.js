bf8edf7fd1c33e84eccbfdb3f54c3cd596da6fa8
Dmitriy Goncharov <dimko.goncharov@gmail.com>
2015-04-01 06:25:42 +0300

    Bootstrapped application, added SiteController with Dashboard method

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= bootstrap_sprockets
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'site/dashboard'
+
+  # get 'site_controller/index'
+
+  root 'site#dashboard'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
