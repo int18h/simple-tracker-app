a4565b2e5cbf0b782c2ffad15cf6afa2f7971ad7
Dmitriy Goncharov <dimko.goncharov@gmail.com>
2015-04-02 03:18:30 +0300

    Finished Sign Up, Sign In, Sign out features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :require_sign_in
+  
+  private
+    def require_sign_in
+      unless signed_in?
+        flash[:danger] = "You must be signed in to access this section"
+        redirect_to signin_path
+      end
+    end
+    
 end
