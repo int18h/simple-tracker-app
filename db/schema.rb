1fb94ec266eadd3784617e6716439e546ae142d6
Dmitriy Goncharov <dimko.goncharov@gmail.com>
2015-04-03 15:29:05 +0300

    Added model worker to connect teams and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403022138) do
+ActiveRecord::Schema.define(version: 20150403122642) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20150403022138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_index "teams", ["user_id"], name: "index_teams_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -36,5 +34,15 @@ ActiveRecord::Schema.define(version: 20150403022138) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+
+  create_table "workers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "workers", ["team_id"], name: "index_workers_on_team_id"
+  add_index "workers", ["user_id"], name: "index_workers_on_user_id"
 
 end
