6c3cb448753f6aa0f8aea4813d77b9b919e16adb
Dmitriy Goncharov <dimko.goncharov@gmail.com>
2015-04-03 06:01:15 +0300

    Enabled and moved navigation to sidebar

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'childprocess'
 gem 'bootstrap-sass'
 gem 'autoprefixer-rails'
 gem 'bcrypt-ruby'
-
+gem 'will_paginate', '~> 3.0.6'
 group :doc do
   gem 'sdoc', require: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401214512) do
+ActiveRecord::Schema.define(version: 20150403022138) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "teams", ["user_id"], name: "index_teams_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
