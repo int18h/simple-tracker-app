024c572a1c01049532f1422d19ec1d361efc0dcd
Dmitriy Goncharov <dimko.goncharov@gmail.com>
2015-04-06 18:55:20 +0300

    Updating edit team view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
-
+  include CurrentTeam
   # GET /teams
   # GET /teams.json
   def index
@@ -64,10 +64,10 @@ class TeamsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params[:id])
-    end
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_team
+    #   @team = Team.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,2 +1,19 @@
 class WorkersController < ApplicationController
+  skip_before_action :require_sign_in
+  before_action :set_team, only: [:create]
+  before_action :set_worker, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @workers = Worker.all
+  end
+
+  def set_worker
+    worker = Worker.find(:params[:id])
+  end
+
+  def worker_params
+    params.require(:worker).permit(:team_id, :user_id)
+  end
+
+  private :set_worker, :worker_params
 end
